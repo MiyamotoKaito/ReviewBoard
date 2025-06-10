7c956bf3a00d1633cf1bfd8cfa4a9728bb658b0f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-06-11 01:17:22 +0900

    <fix>GameStartButtonを修正

---
+++ Assets/Script/Animation/GameStartButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;//シーン切り替えに必要
+
+public class GameStartButton: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(Push);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Push()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+}
