cfa2e0bd4eb095583b34f42677e19d415c0a4d10
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-06-18 16:23:41 +0900

    <add>セレクトボタンを追加

--- Assets/Script/System/SelectBotton.cs
+++ Assets/Script/System/SelectBotton.cs
@@ -8,30 +8,18 @@ using UnityEditor;
 public class SelectButton : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] GameObject selectUIPanel;
     [SerializeField] Button stage1Button;
     [SerializeField] Button stage2Button;
     [SerializeField] Button stage3Button;
     void Start()
     {
         stage1Button.onClick.AddListener(() => Stage1UI());
-        stage1Button.onClick.AddListener(() => Stage2UI());
-        stage1Button.onClick.AddListener(() => Stage3UI());
+        stage2Button.onClick.AddListener(() => Stage2UI());
+        stage3Button.onClick.AddListener(() => Stage3UI());
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        ShowUI();
-    }
 
-    private void ShowUI()
-    {
-        if (selectUIPanel != null)
-        {
-            selectUIPanel.SetActive(true);
-        }
-    }
 
     private void Stage1UI()
     {
